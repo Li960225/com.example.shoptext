bb2b188c0c0084df13feb3653c5047b35cc07a6e
Li960225 <3390791454@qq.com>
2020-12-25 22:01:40 +0800

    第六次提交

--- app/src/main/java/com/example/shoptext/fragment/HomeFragment.java
+++ app/src/main/java/com/example/shoptext/fragment/HomeFragment.java
@@ -5,20 +5,22 @@ import android.os.Bundle;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.alibaba.android.vlayout.DelegateAdapter;
 import com.alibaba.android.vlayout.VirtualLayoutManager;
 import com.alibaba.android.vlayout.layout.ColumnLayoutHelper;
+import com.alibaba.android.vlayout.layout.GridLayoutHelper;
 import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
+import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
 import com.example.mylibrary.base.BaseFragment;
 import com.example.mylibrary.base.BasePresenter;
 import com.example.mylibrary.base.BaseView;
 import com.example.shoptext.R;
+import com.example.shoptext.adapter.FifthAdapter;
 import com.example.shoptext.adapter.FirstAdapter;
+import com.example.shoptext.adapter.FourthAdapter;
 import com.example.shoptext.adapter.SecondAdapter;
 import com.example.shoptext.adapter.ThirdAdapter;
 import com.example.shoptext.bean.HomeBean;
@@ -35,6 +37,9 @@ public class HomeFragment extends BaseFragment<HomePresenterImp> implements Home
     private SecondAdapter secondAdapter;
     private ArrayList<HomeBean.DataBean.ChannelBean> channelBeans;
     private ThirdAdapter thirdAdapter;
+    private ArrayList<HomeBean.DataBean.BrandListBean> brandListBeans;
+    private FourthAdapter fourthAdapter;
+    private FifthAdapter fifthAdapter;
 
     @Override
     protected void initData() {
@@ -53,11 +58,45 @@ public class HomeFragment extends BaseFragment<HomePresenterImp> implements Home
         firstlayout();
         secondlayout();
         thirdlayout();
-        DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager, true);
+        fourthlayout();
+        fifthlayout();
+        DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager, false);
         delegateAdapter.addAdapter(firstAdapter);
         delegateAdapter.addAdapter(secondAdapter);
         delegateAdapter.addAdapter(thirdAdapter);
+        delegateAdapter.addAdapter(fourthAdapter);
+        delegateAdapter.addAdapter(fifthAdapter);
         rv_home.setAdapter(delegateAdapter);
+    }
+
+    private void fifthlayout() {
+        /**
+         设置Grid布局
+         */
+        GridLayoutHelper gridLayoutHelper = new GridLayoutHelper(2);
+
+        // 公共属性
+        gridLayoutHelper.setItemCount(4);// 设置布局里Item个数
+        gridLayoutHelper.setBgColor(Color.WHITE);// 设置背景颜色
+
+        // gridLayoutHelper特有属性（下面会详细说明）
+        gridLayoutHelper.setWeights(new float[]{50, 50});//设置每行中 每个网格宽度 占 每行总宽度 的比例
+        gridLayoutHelper.setVGap(5);// 控制子元素之间的垂直间距
+        gridLayoutHelper.setHGap(5);// 控制子元素之间的水平间距
+        gridLayoutHelper.setAutoExpand(false);//是否自动填充空白区域
+        gridLayoutHelper.setSpanCount(2);// 设置每行多少个网格
+        brandListBeans = new ArrayList<>();
+        fifthAdapter = new FifthAdapter(getActivity(), gridLayoutHelper, brandListBeans);
+    }
+
+    private void fourthlayout() {
+        SingleLayoutHelper singleLayoutHelper = new SingleLayoutHelper();
+
+        // 公共属性
+        singleLayoutHelper.setItemCount(3);// 设置布局里Item个数
+        singleLayoutHelper.setBgColor(Color.WHITE);// 设置背景颜色
+        singleLayoutHelper.setAspectRatio(6);// 设置设置布局内每行布局的宽与高的比
+        fourthAdapter = new FourthAdapter(getActivity(),singleLayoutHelper);
     }
 
     private void thirdlayout() {
@@ -122,6 +161,9 @@ public class HomeFragment extends BaseFragment<HomePresenterImp> implements Home
         List<HomeBean.DataBean.ChannelBean> channel = data.getChannel();
         channelBeans.addAll(channel);
         thirdAdapter.notifyDataSetChanged();
+        List<HomeBean.DataBean.BrandListBean> brandList = data.getBrandList();
+        brandListBeans.addAll(brandList);
+        fifthAdapter.notifyDataSetChanged();
     }
 
     @Override
