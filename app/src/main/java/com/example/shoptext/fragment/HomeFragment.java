778e18bcf65f9b670f78646f8c466e4bc72d34d6
Li960225 <3390791454@qq.com>
2020-12-25 13:52:36 +0800

    第二次提交

--- app/src/main/java/com/example/shoptext/fragment/HomeFragment.java
+++ app/src/main/java/com/example/shoptext/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.shoptext.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,11 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        RecyclerView rv_home = view.findViewById(R.id.rv_home);
     }
 }
